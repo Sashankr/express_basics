d8ad3f8fd660a40b288b4467a074c050b9eb8b55
Sashankr <sashankr97@gmail.com>
2023-09-07 12:30:55 +0530

    Updated routes for expressin app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/users", (req, res, next) => {
   console.log("middleware 1");
-  next();
+  res.send(`
+    <h1>Users List</h1>
+    <a href="/">Home</a>    
+  `);
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("middleware 2");
-  res.send("<h1>Express Basics</h1>");
+  res.send(`
+    <h1>Homepage</h1>
+    <a href="/users">Users</a>
+  `);
 });
 
 app.listen(3000, () => {
